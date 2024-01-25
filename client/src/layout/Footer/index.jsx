d18465868ca8e790007edde5ec7e9796c9ca7192
togrulbadirov <toghrul4242@gmail.com>
2024-01-25 17:43:54 +0400

    updated!

--- client/src/layout/Footer/index.jsx
+++ client/src/layout/Footer/index.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 import "./index.scss"
+import { Link } from 'react-router-dom'
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 const Footer = () => {
   return (
-    <p>©2018 All Rights Reserverd. This template is made with  by Colorlib</p>
+    <>
+    <div className="footer-top">
+        <div className="container">
+        <div className="links">
+        <Link className='link'>Blog</Link>
+        <Link className='link'>FAQs</Link>
+        <Link className='link'>Contact us</Link>
+        </div>
+        <div className="icons">
+        <FaFacebook />
+        <FaTwitter />
+        <FaInstagram />
+        </div>
+        </div>
+    </div>
+    <div className="container footerr">
+    ©2018 All Rights Reserverd. This template is made with  by Colorlib
+    </div>
+    </>
   )
 }
 
